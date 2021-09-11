dbdf9f5b1f742c1a00418379e19288b707609eed
MFernandez6 <miguelfernandez023@gmail.com>
2021-09-11 15:20:21 -0400

    some new commits bro that deploys to GH pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import profileImage from './img/picture.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>REACT APP BRO</h1>
+      <h1>MIGUEL</h1>
+      <img src={profileImage} alt="profile-image" />
     </div>
   );
 }
